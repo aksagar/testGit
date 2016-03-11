90d7d1e49534032d586f4a2808df8451f7c50701
sagar <abdul.kader.sagar@gmail.com>
2016-03-11 07:56:58 -0600

    lets see if this worls

--- test/src/test/mymain.java
+++ test/src/test/mymain.java
@@ -3,5 +3,19 @@ package test;
 public class mymain {
 	public static void main(String []args){
 		System.out.println("this is for testing git functionality");
+		printBranch1();
+	}
+	
+	
+	static void printBranch1(){
+		System.out.println("this is for branch 1");
+	}
+	
+	static void printBranch2(){
+		System.out.println("this is for branch 1");
+	}
+	
+	static void printBranch3(){
+		System.out.println("this is for branch 1");
 	}
 }
